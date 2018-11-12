79a6092386a242de46e3018938fa2ad43394400e
Devesh <tyndallraydv@gmail.com>
2018-11-12 16:22:12 -0500

    GameController Changed

--- BGame/Models/BGameDbContext.cs
+++ BGame/Models/BGameDbContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using BGame.Models.UserModels;
 namespace BGame.Models
 {
     public class BGameDbContext: DbContext
