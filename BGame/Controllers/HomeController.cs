e9e2fb7e86c41fd4815f7e99ab704819f1d3447b
CorpOnThron <koterator@gmail.com>
2018-11-12 14:54:09 -0500

    Views added to controller

--- BGame/Controllers/HomeController.cs
+++ BGame/Controllers/HomeController.cs
@@ -12,5 +12,24 @@ namespace BGame.Controllers
         {
             return View();
         }
+
+        public IActionResult BuyPage()
+        {
+            return View();
+        }
+
+        public IActionResult SellPage()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
     }
 }
