09159afae9d4b5f89185e1eb141989002d2dbe50
CorpOnThron <koterator@gmail.com>
2018-11-12 15:19:46 -0500

    User Models halfway

---
+++ BGame/Models/UserModels/UserInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BGame.Models.UserModels
+{
+    public interface IUserInterface
+    {
+        IQueryable<User> Games { get; }
+        void Add(User obj);
+
+    }
+}
