9197919421844633b94e95d670785e1999e56d6b
wxl-007 <251280943@qq.com>
2018-11-12 17:30:32 -0500

    add migrations files and fix bugs

--- BGame/Models/GameModels/GameItem.cs
+++ BGame/Models/GameModels/GameItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace BGame.Models
     public class GameItem
     {
         public string Name { get; set; }
+        [Key]
         public int GameItemId { get; set; }
         public string Description { get; set; }
         //public DateTime GameTime { get; set; }
