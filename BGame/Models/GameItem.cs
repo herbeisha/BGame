cc594fadbf7c6c80745cec92bc8da7306dea1724
Devesh <tyndallraydv@gmail.com>
2018-11-12 14:52:44 -0500

    GameItem Model added

---
+++ BGame/Models/GameItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BGame.Models
+{
+    public class GameItem
+    {
+        public string Name;
+        public int GameItemId;
+
+    }
+}
